53aa7c00ffa52968cabae1de5650aa138bdd9f5c
Morvin-Ian <morvinian@gmail.com>
2023-02-17 15:50:41 -0800

    6 & 7

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: print 0-9 using putchar
+*              while using int variable
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int counter;
+
+	for (counter = 0; counter < 10; counter++)
+	{
+		/*convert digit to ASCII representation*/
+		putchar(counter + '0');
+		counter++;
+	}
+	putchar('\n');
+	return (0);
+}
