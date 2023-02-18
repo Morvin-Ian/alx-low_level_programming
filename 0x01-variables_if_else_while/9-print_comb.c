334683f3ecd55b3c8d227ff0fa8fcd568c303605
Morvin-Ian <morvinian@gmail.com>
2023-02-17 16:53:18 -0800

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: print 0-9 and separate with a comma
+* Return: Always 0 (Sucesss)
+*/
+
+int main(void)
+{
+	int counter;
+
+	for (counter = 0; counter < 10; counter++)
+	{
+		putchar(counter + 48);
+		if (counter != 9)
+		{
+			putchar(",");
+			putchar(" ");
+		}
+
+	}
+	putchar("\n");
+	return (0);
+}
