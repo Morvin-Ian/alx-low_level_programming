39cb491d0ade0ce0df9bcf5a7d20cd6e51abe40c
Morvin-Ian <morvinian@gmail.com>
2023-02-21 15:10:16 -0800

    absolute final

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -11,16 +11,12 @@
 
 int _abs(int n)
 {
-	if (n > 0)
+	if (n < 0)
 	{
-		return (n);
-	}
-	else if (n == 0)
-	{
-		return (0);
+		return (n + (n*2));
 	}
 	else
 	{
-		return (n + (n * 2));
+		return (n);
 	}
 }
