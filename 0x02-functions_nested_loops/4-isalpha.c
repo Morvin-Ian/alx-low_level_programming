1c7663eef44d96e3a33641b0d2908459f6450514
Morvin-Ian <morvinian@gmail.com>
2023-02-21 14:13:29 -0800

    errors

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,16 +2,16 @@
 
 /**
  * _isalpha - function to check if
- *           character is lowercase, uppercase, or a number
+ *           character is lowercase, uppercase
  *
  * @c: checks input of function
  *
- * Return: returns 1 if `c` is lowercase, upper or number
+ * Return: returns 1 if `c` is lowercase, upper
  *         otherwise always 0 (Success)
 */
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122 || c >= 0 && c <= 9 || c >= 65 && c <= 90)
+	if (c >= 97 && c <= 122 || c >= 65 && c <= 90)
 	{
 		return (1);
 	}
