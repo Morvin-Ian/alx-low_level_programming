cec6e495e7d8b1b6bce291d8630851ce9d299dd0
Morvin-Ian <morvinian@gmail.com>
2023-02-21 15:03:50 -0800

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _abs - function to check if
+ *       a value is not abs and turns it to abs
+ *
+ * @int Data type as : Argument of the function
+ *
+ * Return: AN basolute value or arg int
+*/
+
+int _abs(int)
+{
+	if (int > 0)
+	{
+		return (int);
+	}
+	else if (int == 0
+	{
+		return (0);
+	}
+	else
+	{
+		return (int + (int * 2));
+	}
+}
