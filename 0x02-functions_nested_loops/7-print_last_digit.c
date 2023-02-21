8f44d1de8883578867459b18b82a75d4d2e7282b
Morvin-Ian <morvinian@gmail.com>
2023-02-21 15:46:57 -0800

    last

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,13 +12,15 @@
 int print_last_digit(int n)
 {
 
+	int last_digit;
+
 	if (n < 0)
 	{
-		int last_digit = -1 * (n % 10);
+		last_digit = -1 * (n % 10);
 	{
 	else
 	{
-		int last_digit = (n % 10);
+		last_digit = (n % 10);
 	}
 
 	_putchar('0' + last_digit);
