ba57ca3ce6622af936c77ba58f7ab6b2ae8d4045
Morvin-Ian <morvinian@gmail.com>
2023-02-23 10:54:30 -0800

    mul

---
+++ 0x04-more_functions_nested_loops/mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*_isdigit - function that verifies if a  character is a digit or not
+*@a: arg 1
+*@b: arg 2
+*Return: a multiple of two args
+*/
+
+int mul(int a, int b)
+{
+	return a * b;
+}
