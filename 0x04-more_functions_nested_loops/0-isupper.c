0f220faa69e4eee1317429b3ea5cac6ccf970f06
Morvin-Ian <morvinian@gmail.com>
2023-02-23 10:18:23 -0800

    uper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _isupper - adds two integers
+ * @c: integer 1
+ *
+ * Description: Checks if a number is upper
+ * Return: 1 or 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 33 && c <= 58)
+		return (1);
+	else
+		return (0)
+}
