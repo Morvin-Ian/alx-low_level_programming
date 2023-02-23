dd779c0f4366bd5e3e5ef1dc7daddee8e442b252
Morvin-Ian <morvinian@gmail.com>
2023-02-23 11:13:14 -0800

    err

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,7 +5,7 @@
 
 void print_numbers(void)
 {
-	for (char i = 48; i <= 57; i++)
+	for (int i = 48; i <= 57; i++)
 	{
 		_putchar(i)
 	}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*print_numbers - function that prints 0-9
+*/
+
+void print_most_numbers(void)
+{
+        for (char i = 48; i <= 57; i++)
+        {
+                _putchar(i)
+        }
+        _putchar('\n');
+}
