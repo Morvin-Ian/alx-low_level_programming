c87d4acce226d12b2c470e350080a05cd55dddaa
Morvin-Ian <morvinian@gmail.com>
2023-02-23 12:17:39 -0800

    radar

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*print_line - prints a line
+*@n: Argument of the function
+*Return: returns nothing
+*/
+
+void print_line(int n)
+{
+	int counter;
+
+	if (n > 0)
+	{
+		for (counter = 0; counter <= n; counter++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
