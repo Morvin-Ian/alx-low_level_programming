206986218870f4b5ac1f2923c666439884305dc1
Morvin-Ian <morvinian@gmail.com>
2023-02-27 10:30:29 -0800

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * swap_int - takes a pointer to an int parameter
+ *               and updates the value
+ *
+ * @a: int parameter
+ * @b: second arg
+ *
+ * Return: Nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
