d8762348edbcbdf267d6e6f5ebdd83046cdc0bc8
Morvin-Ian <morvinian@gmail.com>
2023-02-27 11:24:47 -0800

    root

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+/**
+ * rev_string - reverses a string
+ *
+ * @s: string parameter input
+ *
+ * Return: Nothing
+*/
+
+void rev_string(char *s)
+{
+	int k, n;
+	char ch;
+
+	for (k = 0; s[k] != '\0'; ++l)
+		;
+
+	for (n = 0; n < l / 2; ++n)
+	{
+		ch = s[n];
+		s[n] = s[k - 1 - n];
+		s[k - 1 - n] = ch;
+	}
+
+}
