9c7bef381091da80ce113bd6513a36623cc917d6
Morvin-Ian <morvinian@gmail.com>
2023-03-02 10:56:15 -0800

    fix

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 #include <stdio.h>
 /**
  * infinite_add - adds two numbers
@@ -50,6 +50,5 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	if (*(r) == '0')
 		return (r + 1);
 	else
-		
-return (r);
+		return (r);
 }
